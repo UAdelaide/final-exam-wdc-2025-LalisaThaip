a45080cb8b08103dc88b72d48f2abad4774fe32b
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:18:22 +0000

    [automatic commit] 2025-06-20 03:18:22 UTC 122.150.232.17

--- part1/app.js
+++ part1/app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
       });
 
       // Create a table if it doesn't exist
-     \await db.execute(`
+      await db.execute(`
         CREATE TABLE Users (
             user_id INT AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(50) UNIQUE NOT NULL,
@@ -61,17 +61,11 @@ app.use(express.static(path.join(__dirname, 'public')));
             role ENUM('owner', 'walker') NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
-    `);
+      `);
 
-      await db.execute(`
-            CREATE TABLE Dogs (
-                dog_id INT AUTO_INCREMENT PRIMARY KEY,
-                owner_id INT NOT NULL,
-                name VARCHAR(50) NOT NULL,
-                size ENUM('small', 'medium', 'large') NOT NULL,
-                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-            );
-     `);
+        await db.execute(`
+
+        `);
 
       // Insert data if table is empty
       const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
