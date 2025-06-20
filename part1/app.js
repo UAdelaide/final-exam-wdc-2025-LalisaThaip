693d815b546fa4ddf3bb72a5309d8cba8bb3210a
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:12:19 +0000

    [automatic commit] 2025-06-20 03:12:19 UTC 122.150.232.47

--- part1/app.js
+++ part1/app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
       });
 
       // Create the database if it doesn't exist
-      await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+      await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
       await connection.end();
 
       // Now connect to the created database
