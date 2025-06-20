58325d2d0af2df079bf36835d0e5200be22dd13a
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:36:35 +0000

    [automatic commit] 2025-06-20 03:36:35 UTC 122.150.232.47

--- part1/app.js
+++ part1/app.js
@@ -196,7 +196,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/dogs', async (req,res) => {
     try{
-        const [dogs] = await db.execute(`SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        const [dogs] = await db.execute(`
+        SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id`)
         SELECT
