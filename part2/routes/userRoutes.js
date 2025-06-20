b7096d7a03341f09840b8cf6f893b3904bbd5298
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 12:52:40 +0000

    [automatic commit] 2025-06-20 12:52:40 UTC ;; communications error to 208.67.222.222#53: timed out 122.150.232.197

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -31,7 +31,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  res.json(req.session.user);
+  res.json(req.session.user); // Return the user info from session
 });
 
 router.post('logout/', (req,res) => {
