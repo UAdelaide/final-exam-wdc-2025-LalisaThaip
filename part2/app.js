8ecf8aff2dc1125071cf64d56dff082cf14c2b7c
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 07:48:44 +0000

    [automatic commit] 2025-06-20 07:48:44 UTC 122.150.232.197

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret: 'secret-dog',
     resave: false,
-    saveUnitialized: flase
+    saveUnitialized: false
 }));
 
 // Routes
