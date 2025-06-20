3b40fae6ca16339741c0973b5f7c1bcfaac3a814
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 08:06:42 +0000

    [automatic commit] 2025-06-20 08:06:42 UTC 122.150.232.68

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -24,4 +24,12 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.get('/me', (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'Not logged in' });
+  }
+  res.json(req.session.user);
+});
+
+
 module.exports = router;
