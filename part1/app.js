e482b67851be01d5c3721a30c09f8dd312323bd7
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:17:41 +0000

    [automatic commit] 2025-06-20 03:17:41 UTC 122.150.232.47

--- part1/app.js
+++ part1/app.js
@@ -61,7 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
             role ENUM('owner', 'walker') NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
-      `);
+     `);
 
       await db.execute(`
             CREATE TABLE Dogs (
