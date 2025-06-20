04e0a8becef07dd7cb1c5aa835f6d03b0bfa713d
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 07:38:24 +0000

    [automatic commit] 2025-06-20 07:38:24 UTC 122.150.232.17

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -8,7 +8,11 @@ router.post('/login', async (req, res) => {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username - ? AND password_hash = ?
-    `, )
+    `, [usernme, password]);
+
+    if (rows.length ===0) {
+      
+    }
   }
 
 });
