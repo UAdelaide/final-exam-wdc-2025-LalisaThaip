db1c36237504fc0ee2c30073dfbf08a10b8e2c56
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 09:27:18 +0000

    [automatic commit] 2025-06-20 09:27:18 UTC 122.150.232.47

--- part2/app.js
+++ part2/app.js
@@ -28,7 +28,7 @@ const dogRoutes = require('./routes/dogRoute');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/')
+app.use('/api/dogs', dogRoutes); 
 
 // Export the app instead of listening here
 module.exports = app;
