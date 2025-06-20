95b8d83e80ef284fe8485c57b33027171e69523a
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:44:53 +0000

    [automatic commit] 2025-06-20 03:44:53 UTC 122.150.232.197

--- part1/app.js
+++ part1/app.js
@@ -211,7 +211,7 @@ app.get('/api/dogs', async (req,res) => {
 app.get('/api/walkrequests/open', async (req,res) => {
     try {
         const [requests] = await db.execute(`
-            SELECT wr.request_id, d.name as dog_name, wr.e
+            SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.
         `);
     }
 });
