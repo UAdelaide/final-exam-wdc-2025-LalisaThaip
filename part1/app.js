9e6dcf57e293a133da788f1c75fb6eea682bb884
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 04:12:14 +0000

    [automatic commit] 2025-06-20 04:12:14 UTC 122.150.232.47

--- part1/app.js
+++ part1/app.js
@@ -229,7 +229,7 @@ app.get('/api/walkers/summary', async (req,res) => {
         const [summary] = await db.execute(`
             SELECT u.username AS walker_username,
             COUNT(r.rating_id) AS total_ratings,
-            ROUND(AVG())
+            ROUND(AVG(.rating), 1)
 
 
         `);
