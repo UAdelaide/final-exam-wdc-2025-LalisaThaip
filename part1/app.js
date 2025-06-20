8d3e2a7f18fd089c98762cd540f8ee1d00d97be8
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:55:32 +0000

    [automatic commit] 2025-06-20 03:55:32 UTC 122.150.232.17

--- part1/app.js
+++ part1/app.js
@@ -216,7 +216,7 @@ app.get('/api/walkrequests/open', async (req,res) => {
             JOIN Users u on d.owner_id = u.user_id
             WHERE wr.status = 'open'
         `);
-    } catch(err) {wa
+    } catch(err) {
         res.status(500).json({ error: 'Failed to fetch open walkrequests' });
     }
 });
