b323fc21b761228f4ba9ebbbacffcefa9ab5bba7
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:19:03 +0000

    [automatic commit] 2025-06-20 03:19:03 UTC 122.150.232.197

--- part1/app.js
+++ part1/app.js
@@ -74,8 +74,19 @@ app.use(express.static(path.join(__dirname, 'public')));
         `);
 
         await db.execute(`
-        
-    `);
+            CREATE TABLE WalkRequests (
+                request_id INT AUTO_INCREMENT PRIMARY KEY,
+                dog_id INT NOT NULL,
+                requested_time DATETIME NOT NULL,
+                duration_minutes INT NOT NULL,
+                location VARCHAR(255) NOT NULL,
+                status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+            );
+        `);
+
+
 
 
 
