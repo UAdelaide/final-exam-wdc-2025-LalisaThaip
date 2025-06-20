e68e68dc0e3307b0a333cfc9068b076c244307a4
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 07:36:22 +0000

    [automatic commit] 2025-06-20 07:36:22 UTC 122.150.232.17

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -2,5 +2,8 @@ const { router } = require("../../part1/app");
 
 // POSE login using username and password
 router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
-)}
+  
+
+)};
