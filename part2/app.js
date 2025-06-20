c4a768b3b3c1eb5b3cd3640a7098f0ed708e9256
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 07:47:21 +0000

    [automatic commit] 2025-06-20 07:47:21 UTC 122.150.232.47

--- part2/app.js
+++ part2/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 // user express-session middleware
 app.use(session({
-    secret: 'secret-key',
+    secret: 'secret-',
     resave: false,
     saveUno
 })
