71f9a9c7b79898f38c5a541d72cb1da4bf6e5090
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 07:37:43 +0000

    [automatic commit] 2025-06-20 07:37:43 UTC 122.150.232.47

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -6,7 +6,8 @@ router.post('/login', async (req, res) => {
 
   try {
     const [rows] = await db.query(`
-      SELECT user_id, 
+      SELECT user_id, username, role FROM Users
+      WHERE username - 
     `)
   }
 
