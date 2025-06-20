3cbbc8080dd846ca21a31e5d0501641757cc63b8
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:48:38 +0000

    [automatic commit] 2025-06-20 03:48:38 UTC 122.150.232.68

--- part1/app.js
+++ part1/app.js
@@ -213,6 +213,7 @@ app.get('/api/walkrequests/open', async (req,res) => {
         const [requests] = await db.execute(`
             SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.duration_minutes, u.username AS ownerusername
             FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id
+            Join 
             wr.location,
         `);
     }
