27e80a0c8097bb52ea4d54bab25c3103364ff6aa
LalisaThaip <lizs.xrndr@gmail.com>
2025-06-20 03:37:37 +0000

    [automatic commit] 2025-06-20 03:37:37 UTC 122.150.232.17

--- part1/app.js
+++ part1/app.js
@@ -200,7 +200,7 @@ app.get('/api/dogs', async (req,res) => {
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id`)
-        SELECT
+        
     }
 
 });
